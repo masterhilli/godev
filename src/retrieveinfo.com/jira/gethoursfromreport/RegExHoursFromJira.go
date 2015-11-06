ec4a36090b56196c04ec616cb1b47506c1900669
masterhilli <martin.hillbrand@gmail.com>
2015-11-06 16:12:35 +0100

    parsing of text between <tr>asciii</tr>

--- src/retrieveinfo.com/jira/gethoursfromreport/RegExHoursFromJira.go
+++ src/retrieveinfo.com/jira/gethoursfromreport/RegExHoursFromJira.go
@@ -1,9 +1,17 @@
 package jiraRegEx
-
-import regex
-
-
+import "regexp"
 func ParseForTRTR(stringToParse string) string {
-	return "";
+	searchText := regexp.MustCompile("<tr>[[:ascii:]]*</tr>")
+	if searchText.MatchString(stringToParse) == true {
+		submatchOfFoundString := searchText.FindSubmatch([]byte(stringToParse))
+		substring :=  string(submatchOfFoundString[0])
+		return substring[4:len(substring)-5]
+	} else {
+		return "";
+	}
 }
 
+/*
+func HasTablesOnHtml(){
+	Mat
+}*/
--- src/retrieveinfo.com/jira/gethoursfromreport/RegExHoursFromJira_test.go
+++ src/retrieveinfo.com/jira/gethoursfromreport/RegExHoursFromJira_test.go
@@ -12,10 +12,28 @@ func AssertEquals (t *testing.T, actual string, expected string) {
 }
 
 var testString string = "<tr>---</tr>"
-var expected  string = "<tr>---</tr>"
-
 // Test
 func TestTRTRRegExParser(t *testing.T) {
 	actual := ParseForTRTR(testString)
+	expected := "---"
+	AssertEquals (t, actual, expected)
+}
+
+func TestTRTRWithNumAndChars(t *testing.T) {
+	actual := ParseForTRTR("<tr>BLA123BLA</tr>")
+	expected := "BLA123BLA"
+	AssertEquals (t, actual, expected)
+}
+
+
+func TestTRTRNovaluesIn(t *testing.T) {
+	actual := ParseForTRTR("<tr></tr>")
+	expected := ""
+	AssertEquals (t, actual, expected)
+}
+
+func TestTRTRNoMatch(t *testing.T) {
+	actual := ParseForTRTR("<tr</tr>")
+	expected := ""
 	AssertEquals (t, actual, expected)
 }
