1de158733d989f35ac5aece86abbee3a15fb701d
masterhilli <martin.hillbrand@gmail.com>
2015-11-09 12:45:58 +0100

    finally found out the right regexp and start to retrieve the statistics per person and total

--- src/retrieveinfo.com/jira/gethoursfromreport/GenerateHoursPerProject.go
+++ src/retrieveinfo.com/jira/gethoursfromreport/GenerateHoursPerProject.go
@@ -1,10 +1,38 @@
-package jiraRegEx
+package main
 
 
 import	(
 	"fmt"
+    "io/ioutil"
 )
 
+
 func main () {
-	fmt.Printf("Hello World\n")
+    var generateStatitics GenerateHoursPerProject
+    data := generateStatitics.RetrieveJIRAReportStream()
+    fmt.Printf("Data read in from file, len: %d\n", len(data))
+}
+
+type GenerateHoursPerProject struct {
+    
+}
+
+func (ghpp *GenerateHoursPerProject) check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func (ghpp *GenerateHoursPerProject) RetrieveJIRAReportStream() string {
+    data, err := ioutil.ReadFile("./testdata/Report-Jira.html")
+    ghpp.check(err)
+    return string(data)
+}
+
+func (ghpp *GenerateHoursPerProject) getRegExpForTableRowToFindEmployeeNames() string {
+    return "(?is)<tr>.*[<td [[:ascii:]]*>[[:alpha:]]*\\.[[:alpha:]]*</td>]*[[:space:]]*<td class=\"main\"><b>Total</b></td>[[:space:]]*</tr>"
+}
+
+func (ghpp *GenerateHoursPerProject) getRegExpForTableRowToFindTotalTimes() string {
+    return "(?is)<tr>.*[<td [[:ascii:]]*>[[:alpha:]]*\\.[[:alpha:]]*</td>]*[[:space:]]*<td class=\"main\"><b>Total</b></td>[[:space:]]*</tr>"
 }
