117f2ebac5a01a9877cc2ffaae2c9f114f6dfaa6
masterhilli <martin.hillbrand@gmail.com>
2015-12-17 17:09:11 +0100

    solved question 2 of advent code thingy

--- src/adventcode/001_GetRightFloor.go
+++ src/adventcode/001_GetRightFloor.go
@@ -1,13 +1,38 @@
 package getrightfloor
 
-func IsOpeningBracket(bracket string) bool {
-	if bracket == "(" {
+import (
+	"strings"
+)
+
+func IsOpeningBracket(bracket byte) bool {
+	if bracket == byte('(') {
 		return true
 	} else {
 		return false
 	}
 }
 
-func RetrieveFloor(brackets string) int64 {
-	return 0
+func RetrieveFloor(brackets string) int {
+	retVal := strings.Count(brackets, "(")
+	cnt := strings.Count(brackets, ")")
+	return retVal - cnt
+}
+
+func RetrievePosOfFirstBasement(brackets string) int {
+	var bytes []byte = []byte(brackets)
+	var retVal int = 0
+	var i int = 0
+
+	for i = range bytes {
+		if IsOpeningBracket(bytes[i]) {
+			retVal++
+		} else {
+			retVal--
+		}
+
+		if retVal < 0 {
+			return i + 1
+		}
+	}
+	return i + 1
 }
